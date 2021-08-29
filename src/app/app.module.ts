392cc33e8fa1fc425e28bed7e5870be76fc76236
Benoît Hubert <benoithubert@gmail.com>
2021-08-29 12:17:37 +0200

    Break down home page into sidebar & movie-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { MovieListComponent } from './home/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
