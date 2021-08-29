5bfe8eb58f35452e4fe21d9dd3c5ec43a38b25c5
Benoît Hubert <benoithubert@gmail.com>
2021-08-29 13:21:57 +0200

    Populate MOVIES w/ more items & type it

--- src/app/mocks/movies.ts
+++ src/app/mocks/movies.ts
@@ -1,6 +1,6 @@
 import { Movie } from '../interfaces/movie';
 
-export const MOVIES = [
+export const MOVIES: Array<Movie> = [
   {
     id: 1,
     title: 'The Suicide Squad',
@@ -28,5 +28,33 @@ export const MOVIES = [
     picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/lq68Z5htr1bjPd8X0U3PBZRcUij.jpg',
     releaseDate: '2021-08-12',
     rating: 82,
+  },
+  {
+    id: 5,
+    title: 'Narco Sub',
+    picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/7p0O4mKYLIhU2E5Zcq9Z3vOZ4e9.jpg',
+    releaseDate: '2021-07-22',
+    rating: 53,
+  },
+  {
+    id: 6,
+    title: 'Black Widow',
+    picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg',
+    releaseDate: '2021-07-07',
+    rating: 78,
+  },
+  {
+    id: 7,
+    title: 'Space Jam: A New Legacy',
+    picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg',
+    releaseDate: '2021-07-08',
+    rating: 75,
+  },
+  {
+    id: 8,
+    title: 'Free Guy',
+    picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/hEqw9swA8gFJuNjgWYEypwZfkZg.jpg',
+    releaseDate: '2021-08-11',
+    rating: 78,
   }
 ]
