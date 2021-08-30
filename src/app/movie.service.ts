a37758c35c29a9e100213412bf7438c4da938b2e
Benoît Hubert <benoithubert@gmail.com>
2021-08-30 10:12:15 +0200

    Make sorting work using BehaviorSubject

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MOVIES } from 'src/app/mocks/movies';
 import { Movie } from './interfaces/movie';
-import { of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 export enum SortBy {
   rating = 'rating',
@@ -22,24 +22,29 @@ type SortByOrderStrings = keyof typeof SortByOrder;
 })
 export class MovieService {
 
-  private _sortBy: SortBy = SortBy.title;
+  private _sortBy: SortBy = SortBy.rating;
 
-  private movies: Array<Movie> = MOVIES;
+  private movies: Movie[] = MOVIES;
 
-  constructor() { }
+  private movies$: BehaviorSubject<Movie[]>;
+
+  constructor() {
+    this.movies$ = new BehaviorSubject(this.sort());
+  }
 
   set sortBy(sortBy: SortBy) {
     this._sortBy = sortBy;
+    this.movies$.next(this.sort());
   }
 
   get sortBy() {
     return this._sortBy;
   }
 
-  findAll() {
+  sort(): Movie[] {
     const k: SortByOrderStrings = this._sortBy;
     const order: SortByOrder = SortByOrder[k];
-    const movies = this.movies.sort((a, b) => {
+    return this.movies.sort((a: Movie, b: Movie) => {
       console.log(k, order);
       if (a[k] < b[k]) {
         return -1 * order;
@@ -49,7 +54,9 @@ export class MovieService {
       }
       return 0;
     });
-    console.log(movies);
-    return of(movies);
+  }
+
+  findAll() {
+    return this.movies$;
   }
 }
